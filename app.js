b0fe9541c47b81f282d40783bf2b78eb1cb69653
panda1 <blueblue0011@gmail.com>
2022-08-15 14:10:13 -0700

    figuring how the it works. 7 done

--- app.js
+++ app.js
@@ -8,11 +8,28 @@ function get_info (details) {
 
     axios.request({
         url: `https://reqres.in/api/login`,
-        method: `POST`
+        method: `POST`,
         data: {
+            email: `${user_email}`,
+            password: `${user_password}`
 
         }
     }).then(successFunction).catch(failureFunction);
 }
 
+function successFunction (response444) {
+    
+    Cookies.set(`current_user`, `${response444[`data`][`token`]}`);
+ document.body.insertAdjacentHTML("beforeend", `<h1>${response444[`data`][`token`]}</h1>`)
+}
 
+function failureFunction (errro5555) {
+    console.log(`fail`)
+}
+
+
+function my_function(details) {
+    a = 2
+}
+let input = document.getElementById(`inputBox`);
+input.addEventListener(`click`, my_function);
